b8ea676b37fbff284ce9acc5056e316cc38e4cb6
mursid-wabot <97183746+Botwa021@users.noreply.github.com>
2022-10-06 22:26:04 +0700

    Update group-tagall.js

--- plugins/group-tagall.js
+++ plugins/group-tagall.js
@@ -1,17 +1,18 @@
-let handler = async(m, { conn, text, participants }) => {
-  let teks = `⋙ *PESAN DARI ADMIN GROUP* ⋘
-\n *${text ? text : 'Nothing'}*\n\n`
-		      	for (let mem of participants) {
-		            teks += ` @${mem.id.split('@')[0]}\n`
-				}
-                teks += `___________________________________________`
-                conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
+let handler = async(m, { isOwner, isAdmin, conn, text, participants }) => {
+  if (!(isAdmin || isOwner)) {
+                global.dfail('admin', m, conn)
+                throw false
+                }
+  let teks = `${text ? text : ''}\n\n┌──「 *ᴛᴀɢᴀʟʟ* 」──⬣\n`
+  for (let mem of participants) {
+  teks += `│⬡ @${mem.id.split('@')[0]}\n`}
+  teks += `└───────⬣\n`
+  conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 }
-handler.help = ['tagall <pesan>']
+handler.help = ['tagall <message>']
 handler.tags = ['group']
-handler.command = /^(tagall)$/i
+handler.command = /^(t(agall)?)$/i
 
 handler.group = true
-handler.admin = true
 
 module.exports = handler
