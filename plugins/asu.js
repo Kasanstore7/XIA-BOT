691195c351b5045acde22afd70bce94ef2e0ea73
mursid-wabot <97183746+Botwa021@users.noreply.github.com>
2022-10-11 17:28:11 +0700

    Update asu.js

--- plugins/asu.js
+++ plugins/asu.js
@@ -1,11 +1,11 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 let fetch = require("node-fetch")
-let tio = 'Halo Kontol :v'
+let tio = 'мυʀѕι∂ вσт-χмℓ'
  await conn.sendFile(m.chat, 'https://database.tioclkp02.repl.co/Dengarkanlah.mp3', 'Dengarkanlah.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true, contextInfo: {
         externalAdReply: { showAdAttribution: true, title: tio,
- body: wm, sourceUrl: 'https://botcahx.ddns.net', thumbnail: await (await fetch('https://telegra.ph/file/cfbeb870983c988666691.jpg')).buffer()}} 
+ body: wm, sourceUrl: 'https://botcahx.ddns.net', thumbnail: await (await fetch('https://telegra.ph/file/c305f13d22afcfe92474c.jpg')).buffer()}} 
      })
 
 }
