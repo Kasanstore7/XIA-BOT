00bf1926b131386109d32c468ea7c49b97c3c134
mursid-wabot <97183746+Botwa021@users.noreply.github.com>
2022-10-11 17:40:27 +0700

    Update infonya.js

--- plugins/infonya.js
+++ plugins/infonya.js
@@ -1,17 +1,19 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
    let ar = ['Owner', 'Donasi']
    let ras = `Halo @${m.sender.split('@')[0]}`
-    let sel = `Silahkan Pilih Sub Menu Di Bawah Ini`
-    let rs = `Click Here`
+    let sel = `Silahkan Pilih Menu Di Bawah Ini`
+    let rs = `Klik Disini`
 const sections = [ {
 	title: `Pilih Salah Satu Menu `,
 	rows: [
-	   {title: `Speed`, rowId: `#speed `},//, description: ``},
-        {title: `Speedtest`, rowId: `#speedtest `},//, description: ``},
-        {title: `Script`, rowId: `#sc `},//, description: ``},
-         {title: `Thanks`, rowId: `#tqto `},//, description: ``},
-         {title: `Group Official`, rowId: `#gcbot `},//, description: ``},
-         {title: `Website Official`, rowId: `#web `},//, description: ``},
+	   {title: `кєᴄєραтαɴ вσт`, rowId: `#speed `},//, description: `Menampilkan kecepatan bot`},
+        {title: `ѕρєє∂ тєѕт`, rowId: `#speedtest `},//, description: `Menampilkan uji kecepatan server bot`},
+        {title: `ѕσυʀᴄє ᴄσ∂є`, rowId: `#sc `},//, description: `Script bot whatsapp`},
+         {title: `тнαɴкѕ`, rowId: `#tqto `},//, description: `Terima kasih banyak semuanya`},
+         {title: `gʀυρ σƒƒιᴄιαℓ`, rowId: `#gcbot `},//, description: `Grup masih dalam pengembangan`},
+         {title: `ᴡєвѕιтє`, rowId: `#web `},//, description: `Website owner`},
+         {title: `ᴅσɴαѕι`, rowId: `#donasi `},//, description: `Donasi untuk membantu bot, agar selau online`},
+         {title: `ɪɴƒσ мυʀѕι∂`, rowId: `#infomursid `},//, description: ``},
         ]
  } ]
 
