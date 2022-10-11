09c7002b3d083f7927161f043f455c23e6091a28
mursid-wabot <97183746+Botwa021@users.noreply.github.com>
2022-10-11 17:55:27 +0700

    Update main-ok.js

--- plugins/main-ok.js
+++ plugins/main-ok.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
-conn.sendImageAsSticker(m.chat, 'https://database.tioclkp02.repl.co/f61a7c96f3f86b6c5214f.png', m, { packname: "Database Replit", author: "replit @tioclkp02 500gb Room\n\n> No enc, Private Content" })
+conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/f0b1c19aca4215ffb4ac7.jpg', m, { packname: "wa.me/6287855066799", author: "мυʀѕι∂ вσт-χмℓ\n> Instagram : @mursid.st" })
 }
 
-handler.customPrefix = /^(ok)$/i
+handler.customPrefix = /^(ok|okey|otey|okeh)$/i
 handler.command = new RegExp
 
 module.exports = handler
