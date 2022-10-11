e9ba9b2b79692df2439151494908c1a5b41566ea
mursid-wabot <97183746+Botwa021@users.noreply.github.com>
2022-10-11 18:02:59 +0700

    Update main-thanks.js

--- plugins/main-thanks.js
+++ plugins/main-thanks.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
-conn.sendImageAsSticker(m.chat, 'https://database.tioclkp02.repl.co/1240fb4a4d600fb02355b.png', m, { packname: "Sama Sama by", author: "Tio" })
+conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/31a705379b764fa20c5e5.png', m, { packname: "ig @mursid.st\ntiktok @mursid_st", author: "мυʀѕι∂ вσт-χмℓ" })
 }
 
-handler.customPrefix = /^(Terima Kasih|Thanks|tq)$/i
+handler.customPrefix = /^(Terima Kasih|Thanks|tq|makasih|makasih|makaciw)$/i
 handler.command = new RegExp
 
 module.exports = handler
