d1c6fdf9a65f9bf57bf86e11aefa5e8495644f16
mursid-wabot <97183746+Botwa021@users.noreply.github.com>
2022-10-12 18:11:26 +0700

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,16 +1,15 @@
-const { tiktokdl, tiktokdlv2, tiktokdlv3 } = require('@bochilteam/scraper')
-let handler = async (m, { conn, args, usedPrefix, command, isOwner }) => {
-	if (!args[0]) throw `Link tiktoknya mana?\n\ncontoh:\n${usedPrefix}${command} https://vm.tiktok.com/ZGJAmhSrp/`
-    tiktokdlv2(args[0]).then(r => {
-    let video = r.video.no_watermark
-    conn.sendFile(m.chat, video, '', `*${wm}*`, m)
-    })
+let fetch = require('node-fetch')
+let axios = require('axios')
+let handler = async (m, { conn, args }) => {
+  if (!args[0]) throw 'Uhm...url nya mana?'
+ 
+await conn.reply(m.chat, `Downloading media from Tiktok`, m)
+let txt = `🚀 *Link:* ${await(await axios.get(`https://tinyurl.com/api-create.php?url=${args[0]}`)).data}` 
+    await conn.send2ButtonVid(m.chat, `https://api.lolhuman.xyz/api/tiktokwm?apikey=SadTeams&url=${args[0]}` , txt, wm, `No Wm`, `.tiktoknowm ${args[0]}`, `Audio`, `.tiktokaudio ${args[0]}`, m)
 }
 handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-handler.limit = true
-handler.group = false
 
-handler.command = /^(tt|tiktok|tik)$/i
+handler.command = /^((tt|tiktok)?(dl)?)$/i
 
 module.exports = handler
