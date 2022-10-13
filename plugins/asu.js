1f2947488ad117ff685df26d5a6ff00e03b94efc
mursid-wabot <97183746+Botwa021@users.noreply.github.com>
2022-10-13 14:01:29 +0700

    Update asu.js

--- plugins/asu.js
+++ plugins/asu.js
@@ -1,16 +1,16 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 let fetch = require("node-fetch")
-let tio = 'мυʀѕι∂ вσт-χмℓ'
+let tio = 'ʜᴀʏᴏ ʙᴀɴɢ ɢᴀʙᴏʟᴇʜ ɢɪᴛᴜ'
  await conn.sendFile(m.chat, 'https://database.tioclkp02.repl.co/Dengarkanlah.mp3', 'Dengarkanlah.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true, contextInfo: {
         externalAdReply: { showAdAttribution: true, title: tio,
- body: wm, sourceUrl: 'https://botcahx.ddns.net', thumbnail: await (await fetch('https://telegra.ph/file/c305f13d22afcfe92474c.jpg')).buffer()}} 
+ body: wm, sourceUrl: 'https://mursidxml.herokuapp.com/docs', thumbnail: await (await fetch('https://telegra.ph/file/5e169bddd254ec1bf180a.jpg')).buffer()}} 
      })
 
 }
 
-handler.customPrefix = /^(crot)$/i
+handler.customPrefix = /^(crot|ahah|ah|crit|anjir)$/i
 handler.command = new RegExp
 
 module.exports = handler
