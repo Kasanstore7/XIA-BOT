23210c32296a40ec1ecc2a6651fb01bd62b0afee
mursid-wabot <97183746+Botwa021@users.noreply.github.com>
2022-10-19 08:57:02 +0700

    Update info-sc.js

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn }) => {
-let ye = `@${m.sender.split`@`[0]}`
+let sid = `@${m.sender.split`@`[0]}`
 let esce = `
-Hai ${ye} Bot Ini Menggunakan Script Github, jangan lupa follow & kasih star nya yaa pak :\n• https://github.com/BOTCAHX/RTXZY-MD
+Hai ${sid} Bot Ini Menggunakan Script Github, jangan lupa follow & kasih star nya yaa pak :\n• https://github.com/Botwa021/M
 `
-conn.sendBut(m.chat, esce, wm3, 'Makasih Mursid', 'thanks', m) 
+conn.sendBut(m.chat, esce, wm3, 'Makasih', 'thanks', m) 
 }
 handler.help = ['sc', 'sourcecode']
 handler.tags = ['info']
