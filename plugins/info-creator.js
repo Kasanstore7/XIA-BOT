623dae5a7def8469f26b2b767d3420ef79923b98
mursid-wabot <97183746+Botwa021@users.noreply.github.com>
2022-10-26 13:58:27 +0700

    Update info-creator.js

--- plugins/info-creator.js
+++ plugins/info-creator.js
@@ -7,10 +7,10 @@ FN: Mursid S
 item.ORG: ᴏᴡɴᴇʀ ʙᴏᴛ
 item1.TEL;waid=6288233832771:6288233832771@s.whatsapp.net
 item1.X-ABLabel:ᴅᴇᴠᴇʟᴏᴘᴇʀ мυʀѕι∂ вσт-χмℓ
-item2.EMAIL;type=INTERNET:ʜᴀɴʏᴀ ᴅᴇᴠᴇʟᴏᴘᴇʀ ʙᴏᴛ ᴡʜᴀᴛꜱᴀᴘᴘ ʙɪᴀꜱᴀ ᴋᴀᴋ
-item2.X-ABLabel:Email
+item2.EMAIL;type=INTERNET:ᴅᴇᴠᴇʟᴏᴘᴇʀ ʙᴏᴛ ᴡʜᴀᴛꜱᴀᴘᴘ
+item2.X-ABLabel:Website
 item3.ADR:;Yogykarta;
-item3.X-ABADR:Yogykarta
+item3.X-ABADR:ʏᴏɢʏᴋᴀʀᴛᴀ
 item3.X-ABLabel:Lokasi
 item4.EMAIL;type=INTERNET:mursidxml@gmail.com
 item4.X-ABLabel:Email
@@ -26,11 +26,11 @@ const sentMsg  = await conn.sendMessage(
         }
     }
 )
-conn.sendMessage(m.chat, { text: `𝘏𝘢𝘭𝘰 𝘬𝘢𝘬 @${await m.sender.split('@')[0]},  𝘪𝘵𝘶 𝘰𝘸𝘯𝘦𝘳𝘬𝘶, 𝘬𝘢𝘭𝘢𝘶 𝘮𝘢𝘶 𝘤𝘩𝘢𝘵 𝘫𝘢𝘯𝘨𝘢𝘯 𝘭𝘶𝘱𝘢 𝘴𝘢𝘭𝘢𝘮 𝘺𝘢𝘩`, mentions: [m.sender] }, { quoted: sentMsg })
+conn.sendMessage(m.chat, { text: `𝙃𝙖𝙮 𝙠𝙖𝙠 *@${await m.sender.split('@')[0]}*, 𝙞𝙩𝙪 𝙤𝙬𝙣𝙚𝙧𝙠𝙪, 𝙟𝙖𝙣𝙜𝙖𝙣 𝙙𝙞𝙨𝙥𝙖𝙢 𝙮𝙖𝙝`, mentions: [m.sender] }, { quoted: sentMsg })
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
 
-handler.command = /^(owner|creator)$/i
+handler.command = /^(owner|creator|dev|own|dv|dp|ct|botown|ownbot)$/i
 
 module.exports = handler
