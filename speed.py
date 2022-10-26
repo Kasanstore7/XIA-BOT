afaa65fc3484b2c76bc792b171447da75f527cfd
mursid-wabot <97183746+Botwa021@users.noreply.github.com>
2022-10-26 12:40:17 +0700

    Update speed.py

--- speed.py
+++ speed.py
@@ -1970,7 +1970,7 @@ def shell():
                 ((results.upload / 1000.0 / 1000.0) / args.units[1],
                  args.units[0]),
                 quiet)
-        printer("\n\n*© TioXd*")
+        printer("\n\n*© мυʀѕι∂ вσт-χмℓ*")
     else:
         printer('Skipping upload test', quiet)
 
