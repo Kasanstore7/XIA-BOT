56ff8a511fc9398e51dd2d81004a9b3ab2b12c8f
mursid-xml <97183746+Botwa021@users.noreply.github.com>
2022-10-28 10:27:55 +0700

    Update downloader-mediafire.js

--- plugins/downloader-mediafire.js
+++ plugins/downloader-mediafire.js
@@ -6,11 +6,13 @@ let mdjon = args.join(' ')
 res = await mediafireDl(mdjon)
 result = `「 *ᴍᴇᴅɪᴀꜰɪʀᴇ ᴅᴏᴡɴʟᴏᴀᴅ* 」
 *Data Berhasil Didapatkan!*
+
 *Nama* : ${res[0].nama}
 *Size File* : ${res[0].size}
 *Media Link* : ${res[0].link}
 *Powered By* : Mursid S
-_Tunggu Proses Upload Media Beberapa Detik_`
+
+_Tunggu Proses Upload Media Memerlukan Waktu Beberapa Detik_`
 m.reply(result)
 //await sleep(100)
 conn.sendFile(m.chat, res[0].link, res[0].nama, null, m, false, {asDocument:true, mimetype:res[0].mime})
