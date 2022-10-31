4993fcce1819f7402d1b5d5c3521af6850eb78d1
mursid-xml <97183746+Botwa021@users.noreply.github.com>
2022-10-31 20:15:21 +0700

    Update downloader-ytv.js

--- plugins/downloader-ytv.js
+++ plugins/downloader-ytv.js
@@ -2,7 +2,7 @@ let fetch = require ('node-fetch')
 let { youtubeSearch } = require ('@bochilteam/scraper')
 let handler = async (m, { conn, text }) => {
   if (!text) throw 'Url nya mana?'
-  m.reply('_Proses..._')
+  m.reply('Loading...')
   let vid = (await youtubeSearch(text)).video[0]
   let { title, description, thumbnail, videoId, durationH, durationS, viewH, publishedTime } = vid
   let url = 'https://www.youtube.com/watch?v=' + videoId
@@ -11,6 +11,6 @@ let ytLink = `https://botcahx2.ddns.net/?url=${url}&filter=audioandvideo&quality
 }
 handler.help = ['ytv'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-handler.command = /^(ytv|ytvideo|ytmp3)$/i
+handler.command = /^(ytv|ytvideo|ytmp4)$/i
 
 module.exports = handler
