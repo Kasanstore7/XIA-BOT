f36151777aa49783fc119544ca4a725bd213ea4d
mursid-xml <97183746+Botwa021@users.noreply.github.com>
2022-10-31 20:17:01 +0700

    Update and rename _autoreply.js to _autobio.js

---
+++ plugins/_autobio.js
@@ -0,0 +1,24 @@
+// anu anunya ada di sini
+let fs = require('fs')
+let fetch = require('node-fetch')
+let handler = m => m
+
+    // update status
+if (new Date() * 1 - setting.status > 1000) {
+        let uptime = clockString(_uptime);
+		let bio = `Aktif selama ${uptime}`
+		await conn.setBio(bio).catch(_ => _)
+        setting.status = new Date() * 1
+    }
+
+}
+
+module.exports = handler
+
+function clockString(ms) {
+  let d = isNaN(ms) ? '--' : Math.floor(ms / 86400000)
+  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
+  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+  return [d, ' Hari ', h, ' Jam ', m, ' Menit ', s, ' Detik '].map(v => v.toString().padStart(2, 0)).join('')
+}
