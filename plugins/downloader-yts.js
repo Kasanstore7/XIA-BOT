7749b0c7b25c91938914c33b739be0d9ff7fd558
mursid-xml <97183746+Botwa021@users.noreply.github.com>
2022-10-31 20:14:28 +0700

    Update downloader-yts.js

--- plugins/downloader-yts.js
+++ plugins/downloader-yts.js
@@ -1,34 +1,26 @@
-import { youtubeSearch } from '@bochilteam/scraper'
-let handler = async (m, { conn, usedPrefix, text }) => {
+let yts = require('yt-search')
+let handler = async (m, { text }) => {
   if (!text) throw 'Cari apa?'
-  const { video, channel } = await youtubeSearch(text)
-  const listSections = []
-  let teks = [...video, ...channel].map(v => {
+  let results = await yts(text)
+  let teks = results.all.map(v => {
     switch (v.type) {
-      case 'video': {
-        listSections.push([`${v.title}`, [
-          ['Video 🎥', `${usedPrefix}ytv ${v.url} yes`, `download ${v.title} (${v.url})`],
-          ['Audio 🎧', `${usedPrefix}yta ${v.url} yes`, `download ${v.title} (${v.url})`]
-        ]])
-        return `
-📌 *${v.title}* (${v.url})
-⌚ Duration: ${v.durationH}
-⏲️ Uploaded ${v.publishedTime}
-👁️ ${v.view} views
+      case 'video': return `
+*${v.title}* (${v.url})
+Duration: ${v.timestamp}
+Uploaded ${v.ago}
+${v.views} views
       `.trim()
-      }
       case 'channel': return `
-📌 *${v.channelName}* (${v.url})
-🧑‍🤝‍🧑 _${v.subscriberH} (${v.subscriber}) Subscriber_
-🎥 ${v.videoCount} video
+*${v.name}* (${v.url})
+_${v.subCountLabel} (${v.subCount}) Subscriber_
+${v.videoCount} video
 `.trim()
     }
-  }).filter(v => v).join('\n\n========================\n\n')
-  const msg = await m.reply(teks)
-  conn.sendList(m.chat, '📺Youtube Search🔎', '\nDownload List', global.wm, 'Choose', listSections, msg)
+  }).filter(v => v).join('\n========================\n')
+  m.reply(teks)
 }
 handler.help = ['', 'earch'].map(v => 'yts' + v + ' <pencarian>')
-handler.tags = ['tools']
+handler.tags = ['tools', 'internet', 'downloader']
 handler.command = /^yts(earch)?$/i
 
-export default handler
+module.exports = handler
