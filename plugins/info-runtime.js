5748b43279f932311a520f9da455885b8ae51710
mursid-xml <97183746+Botwa021@users.noreply.github.com>
2022-11-01 08:43:06 +0700

    Update info-runtime.js

--- plugins/info-runtime.js
+++ plugins/info-runtime.js
@@ -4,10 +4,8 @@ let handler = async (m, { conn }) => {
     let time = require('moment-timezone').tz('Asia/Jakarta').format('HH:mm:ss')
 
 m.reply(`
-
-Bot berjalan selama : ${uptime} 
+Aktif selama : ${uptime} 
 Waktu : ${time} Asia Jakarta
-
     `)
 }
 handler.help = ['runtime']
@@ -17,8 +15,9 @@ handler.command = /^(uptime|runtime)$/i
 module.exports = handler
 
 function clockString(ms) {
+    let d = isNaN(ms) ? '--' : Math.floor(ms / 86400000)
     let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
     let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
     let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-    return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
+    return [d, ' Hari ', h, ' Jam ', m, ' Menit ', s, ' Detik '].map(v => v.toString().padStart(2, 0)).join('')
 }
