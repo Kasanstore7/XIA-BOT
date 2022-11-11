a0932c22f3a4e08a3f9355101d5e4bc9358a2a92
mursid-xml <97183746+Botwa021@users.noreply.github.com>
2022-11-11 13:24:18 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -711,14 +711,14 @@ module.exports = {
                             let wel = API('males', '/welcome2', {
                                 profile: pp,
                                 username: await this.getName(user),
-                                background: 'https://i.ibb.co/z2QQnqm/wp.jpg',
+                                background: 'https://telegra.ph/file/74726473af23983097b0c.jpg',
                                 groupname: await this.getName(id),
                                 membercount: groupMetadata.participants.length
                             })
                             let lea = API('males', '/goodbye2', {
                                 profile: pp,
                                 username: await this.getName(user),
-                                background: 'https://i.ibb.co/z2QQnqm/wp.jpg',
+                                background: 'https://telegra.ph/file/74726473af23983097b0c.jpg',
                                 groupname: await this.getName(id),
                                 membercount: groupMetadata.participants.length
                             })
@@ -761,16 +761,16 @@ Untuk mematikan fitur ini, ketik
 
 global.dfail = (type, m, conn) => {
     let msg = {
-        rowner: 'Perintah ini hanya dapat digunakan oleh _*Owner!*_',
-        owner: 'Perintah ini hanya dapat digunakan oleh _*Owner Bot*_!',
+        rowner: 'Perintah ini hanya dapat digunakan oleh _*Tim Discussion!1!1!*_',
+        owner: 'Perintah ini hanya dapat digunakan oleh _*Tim Discussion!1!1!*_',
         mods: 'Perintah ini hanya dapat digunakan oleh _*Moderator*_ !',
         premium: 'Perintah ini hanya untuk member _*Premium*_ !',
-        group: 'Perintah ini hanya dapat digunakan di grup!',
-        private: 'Perintah ini hanya dapat digunakan di Chat Pribadi!',
+        group: 'Perintah ini hanya dapat digunakan di _*Grup*_!',
+        private: 'Perintah ini hanya dapat digunakan di _*Chat Pribadi*_!',
         admin: 'Perintah ini hanya untuk *Admin* grup!',
         botAdmin: 'Jadikan bot sebagai *Admin* untuk menggunakan perintah ini!',
-        unreg: 'Silahkan daftar untuk menggunakan fitur ini dengan cara mengetik:\n\n*#daftar nama.umur*\n\nContoh: *#daftar mark.20*',
-        restrict: 'Fitur ini di *disable*!'
+        unreg: 'Silahkan daftar untuk menggunakan fitur ini dengan cara mengetik:\n\n*#daftar nama.umur*\n\nContoh: *#daftar Mursid Ganteng.18*',
+        restrict: 'Fitur ini di _*disable*_!'
     }[type]
     if (msg) return m.reply(msg)
 }
