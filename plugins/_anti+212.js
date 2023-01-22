972c50ade541d32df03cd7351dc953eded832061
Mshunone <100399123+Mshunone@users.noreply.github.com>
2023-01-22 23:33:42 +0700

    Update _anti+212.js

--- plugins/_anti+212.js
+++ plugins/_anti+212.js
@@ -9,5 +9,37 @@ handler.before = async function (m) {
    	global.db.data.users[m.sender].banned = true
    } 
     }
-
+   if (m.sender.startsWith('90' || '90')) {
+   	global.db.data.users[m.sender].banned = true
+   } 
+    }
+    if (m.sender.startsWith('91' || '91')) {
+   	global.db.data.users[m.sender].banned = true
+   } 
+    }
+    if (m.sender.startsWith('92' || '92')) {
+   	global.db.data.users[m.sender].banned = true
+   } 
+    }
+    if (m.sender.startsWith('968' || '968')) {
+   	global.db.data.users[m.sender].banned = true
+   } 
+    }
+    if (m.sender.startsWith('52' || '52')) {
+   	global.db.data.users[m.sender].banned = true
+   } 
+    }
+    if (m.sender.startsWith('54' || '54')) {
+   	global.db.data.users[m.sender].banned = true
+   } 
+    }
+    if (m.sender.startsWith('55' || '55')) {
+   	global.db.data.users[m.sender].banned = true
+   } 
+    }
+    if (m.sender.startsWith('51' || '51')) {
+   	global.db.data.users[m.sender].banned = true
+   } 
+    }
+    
 module.exports = handler
