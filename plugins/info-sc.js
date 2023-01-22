3b81dac8e829b95682d6729a106d27b33c80bb0e
Mshunone <100399123+Mshunone@users.noreply.github.com>
2023-01-22 21:20:53 +0700

    Update info-sc.js

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn }) => {
 let sid = `@${m.sender.split`@`[0]}`
 let esce = `
-Hai ${sid} \nʙᴏᴛ ɪɴɪ ᴍᴇɴɢɢᴜɴᴀᴋᴀɴ ꜱᴄʀɪᴘᴛ ɢɪᴛʜᴜʙ, ᴊᴀɴɢᴀɴ ʟᴜᴘᴀ ꜰᴏʟʟᴏᴡ & ᴋᴀꜱɪʜ ꜱᴛᴀʀᴛ ɴʏᴀ ᴘᴀᴋ :\n• https://github.com/BOTCAHX/RTXZY-MD
+Hai ${sid} \nʙᴏᴛ ɪɴɪ ᴍᴇɴɢɢᴜɴᴀᴋᴀɴ ꜱᴄʀɪᴘᴛ:\n• https://github.com/BOTCAHX/RTXZY-MD\n• Di Recode/Base Ulang By: SaxiaBotz\n⫹⫺ Klo Mau Sc Yg Udah Di Recode .owner
 `
 conn.sendBut(m.chat, esce, wm3, 'ᴛᴇʀɪᴍᴀ ᴋᴀꜱɪʜ', 'thanks', m) 
 }
